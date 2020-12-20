63c740a1647db02ea8e2c8763fd932813bf1fe46
gabir-yusuf <gabir.yusuf@gmail.com>
2020-12-20 14:53:22 +0200

    fix

--- trdg/run.sh
+++ trdg/run.sh
@@ -1,229 +1,229 @@
-python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1 
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1 
 mv trdg/fonts/latin/ArchitectsDaughter-Regular.ttf trdg/fonts/used
 mv trdg/fonts/unused/hwWill.ttf trdg/fonts/latin
 
-python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
 mv trdg/fonts/latin/hwWill.ttf trdg/fonts/used
 mv trdg/fonts/unused/hwChase.ttf trdg/fonts/latin
 
 
-python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
 mv trdg/fonts/latin/hwChase.ttf trdg/fonts/used
 mv trdg/fonts/unused/RockSalt-Regular.ttf trdg/fonts/latin
 
 
-python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
 mv trdg/fonts/latin/RockSalt-Regular.ttf trdg/fonts/used
 mv trdg/fonts/unused/hwRuthie.ttf trdg/fonts/latin
 
 
 
-python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
 mv trdg/fonts/latin/hwRuthie.ttf trdg/fonts/used
 mv trdg/fonts/unused/paperdaisy-demo.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/paperdaisy-demo.ttf trdg/fonts/used
-# mv trdg/fonts/unused/ReenieBeanie-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/paperdaisy-demo.ttf trdg/fonts/used
+mv trdg/fonts/unused/ReenieBeanie-Regular.ttf trdg/fonts/latin
 
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/ReenieBeanie-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Caveat-Bold.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/ReenieBeanie-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/Caveat-Bold.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Caveat-Bold.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwJeff.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Caveat-Bold.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwJeff.ttf trdg/fonts/latin
 
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwJeff.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Satisfy-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwJeff.ttf trdg/fonts/used
+mv trdg/fonts/unused/Satisfy-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Satisfy-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/GiveYouGlory-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Satisfy-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/GiveYouGlory-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/GiveYouGlory-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/GloriaHallelujah-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/GiveYouGlory-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/GloriaHallelujah-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/GloriaHallelujah-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/AmaticSC-Bold.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/GloriaHallelujah-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/AmaticSC-Bold.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/AmaticSC-Bold.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwMason.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/AmaticSC-Bold.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwMason.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwMason.ttf trdg/fonts/used
-# mv trdg/fonts/unused/ShadowsIntoLight-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwMason.ttf trdg/fonts/used
+mv trdg/fonts/unused/ShadowsIntoLight-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/ShadowsIntoLight-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwDarleen.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/ShadowsIntoLight-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwDarleen.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwDarleen.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwRachael.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwDarleen.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwRachael.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwRachael.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwJenna.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwRachael.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwJenna.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwJenna.ttf trdg/fonts/used
-# mv trdg/fonts/unused/AmaticSC-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwJenna.ttf trdg/fonts/used
+mv trdg/fonts/unused/AmaticSC-Regular.ttf trdg/fonts/latin
 
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/AmaticSC-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Handlee-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/AmaticSC-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/Handlee-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Handlee-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Cookie-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Handlee-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/Cookie-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Cookie-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/PatrickHand-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Cookie-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/PatrickHand-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/PatrickHand-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwSteve.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/PatrickHand-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwSteve.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwSteve.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwKelsey.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwSteve.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwKelsey.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwKelsey.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwJustin.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwKelsey.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwJustin.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwJustin.ttf trdg/fonts/used
-# mv trdg/fonts/unused/MarckScript-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwJustin.ttf trdg/fonts/used
+mv trdg/fonts/unused/MarckScript-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/MarckScript-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/LeagueScript-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/MarckScript-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/LeagueScript-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/LeagueScript-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Sacramento-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/LeagueScript-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/Sacramento-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Sacramento-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Parisienne-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Sacramento-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/Parisienne-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Parisienne-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwDavid.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Parisienne-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwDavid.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwDavid.ttf trdg/fonts/used
-# mv trdg/fonts/unused/ButterflyKids-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwDavid.ttf trdg/fonts/used
+mv trdg/fonts/unused/ButterflyKids-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/ButterflyKids-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwCharity.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/ButterflyKids-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwCharity.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwCharity.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Anzelia.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwCharity.ttf trdg/fonts/used
+mv trdg/fonts/unused/Anzelia.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Anzelia.ttf trdg/fonts/used
-# mv trdg/fonts/unused/HomemadeApple-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Anzelia.ttf trdg/fonts/used
+mv trdg/fonts/unused/HomemadeApple-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/HomemadeApple-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwSaige.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/HomemadeApple-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwSaige.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwSaige.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Caveat-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwSaige.ttf trdg/fonts/used
+mv trdg/fonts/unused/Caveat-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Caveat-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwLulu.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Caveat-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwLulu.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwLulu.ttf trdg/fonts/used
-# mv trdg/fonts/unused/ArchitectsDaughter-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwLulu.ttf trdg/fonts/used
+mv trdg/fonts/unused/ArchitectsDaughter-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/ArchitectsDaughter-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwMichael.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/ArchitectsDaughter-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwMichael.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwMichael.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwNick.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwMichael.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwNick.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwNick.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwRyan.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwNick.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwRyan.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwRyan.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwAdam.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwRyan.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwAdam.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwAdam.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Pacifico-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwAdam.ttf trdg/fonts/used
+mv trdg/fonts/unused/Pacifico-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Pacifico-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/PermanentMarker-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Pacifico-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/PermanentMarker-Regular.ttf trdg/fonts/latin
 
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/PermanentMarker-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwLindsey.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/PermanentMarker-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwLindsey.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwLindsey.ttf trdg/fonts/used
-# mv trdg/fonts/unused/GreatVibes-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwLindsey.ttf trdg/fonts/used
+mv trdg/fonts/unused/GreatVibes-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/GreatVibes-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/DancingScript-VariableFont_wght.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/GreatVibes-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/DancingScript-VariableFont_wght.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/DancingScript-VariableFont_wght.ttf trdg/fonts/used
-# mv trdg/fonts/unused/Courgette-Regular.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/DancingScript-VariableFont_wght.ttf trdg/fonts/used
+mv trdg/fonts/unused/Courgette-Regular.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/Courgette-Regular.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwSignatures.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/Courgette-Regular.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwSignatures.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwSignatures.ttf trdg/fonts/used
-# mv trdg/fonts/unused/hwErin.ttf trdg/fonts/latin
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwSignatures.ttf trdg/fonts/used
+mv trdg/fonts/unused/hwErin.ttf trdg/fonts/latin
 
-# python trdg/run.py -c 8 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
-# mv trdg/fonts/latin/hwErin.ttf trdg/fonts/used
+python trdg/run.py -c 200 -w 1 -f 100 --font_dir trdg/fonts/latin -t 8 -b 1
+mv trdg/fonts/latin/hwErin.ttf trdg/fonts/used
